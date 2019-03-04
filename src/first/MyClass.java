ebfd8898fb110fe8cbf01df6cdf0f15a3decb449
Srdjan Obradovic <srdjan@3ap.ch>
2019-03-04 12:09:03 +0100

    second commit

--- src/first/MyClass.java
+++ src/first/MyClass.java
@@ -1,5 +1,11 @@
 package first;
 
 public class MyClass {
+	public int multiply(int x, int y) throws IllegalAccessException {
+		if (x > 999) {
+			throw new IllegalAccessException("X should be less than 1000");
+		}
+		return x / y;
+	}
 
 }
--- test/first/MyClassTest.java
+++ test/first/MyClassTest.java
@@ -1,5 +1,27 @@
 package first;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class MyClassTest {
+	MyClass tester = new MyClass();
+
+	@Test
+	public void testExceptionIsThrown() {
+		int x = 10001;
+		int y = 5;
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			tester.multiply(x, y);
+		});
+	}
+
+	@Test
+	public void testMultiply() throws IllegalAccessException {
+		assertEquals(50, tester.multiply(10, 5), "10x5 must be 50");
+
+	}
 
 }
